e0e8baafa3d584843beb34783da868e32ef098e0
jlaluces123 <jlalucescareer123@gmail.com>
2019-01-14 13:30:15 -0800

    Day 1 - Array Append Done

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -116,7 +116,15 @@ void arr_insert(Array *arr, char *element, int index) {
  * Append an element to the end of the array
  *****/
 void arr_append(Array *arr, char *element) {
-
+  // Resize the array if the number of elements is over capacity
+  // or throw an error if resize isn't implemented yet.
+  if (arr->capacity <= arr->count) {
+    resize_array(arr);
+  }
+  // Copy the element and add it to the end of the array
+  arr->elements[arr->count + 1] = element;
+  // Increment count by 1
+  arr->count = arr->count + 1;
 }
 
 /*****
@@ -126,6 +134,25 @@ void arr_append(Array *arr, char *element) {
  * Throw an error if the value is not found.
  *****/
 void arr_remove(Array *arr, char *element) {  
+  // Search for the first occurence of the element and remove it.
+  // Don't forget to free its memory!
+  for (int i = 0; i < arr->count; i++) {
+
+    if (arr->elements[i] == element) { // --> if given element == an item in our elements array
+      free(arr->elements[i]); // --> Free its memory
+      arr->elements[i] = NULL; // --> Set to NULL
+    } else {
+      fprintf(stderr, "Could not find element (remove)");
+      exit(1);
+    }
+
+  }
+  // Shift over every element after the removed element to the left one position
+  for (int i = 0; i < arr->count; i++) {
+
+  }
+  // Decrement count by 1
+  arr->count = arr->count - 1;
 }
 
 
