9d8b29a8be736862fb85208e1fd77a4bb8fa7882
jlaluces123 <jlalucescareer123@gmail.com>
2019-01-14 13:24:56 -0800

    Day 1 - Create Array Done

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -21,10 +21,17 @@ typedef struct Array {
  *****/
 Array *create_array (int capacity) {
   // Allocate memory for the Array struct
+  Array *new_arr = malloc(sizeof(Array));
 
-  // Set initial values for capacity and count
-
+  // Set initial values for capacity and count 
+  new_arr->capacity = capacity;
+  new_arr->count = 0;
   // Allocate memory for elements
+  new_arr->elements = calloc(capacity, sizeof(char *));
+  // --> here we allocate the memory for the amount of elements the array holds
+  // --> AND specify the size of char *
+
+  return new_arr;
 
 }
 
@@ -34,10 +41,6 @@ Array *create_array (int capacity) {
  *****/
 void destroy_array(Array *arr) {
 
-  // Free all elements
-
-  // Free array
-
 }
 
 /*****
@@ -45,15 +48,7 @@ void destroy_array(Array *arr) {
  * from old to new
  *****/
 void resize_array(Array *arr) {
-
-  // Create a new element storage with double capacity
-
-  // Copy elements into the new storage
-
-  // Free the old elements array (but NOT the strings they point to)
-
-  // Update the elements and capacity to new values
-
+  
 }
 
 
@@ -70,27 +65,14 @@ void resize_array(Array *arr) {
  * Throw an error if the index is out of range.
  *****/
 char *arr_read(Array *arr, int index) {
-
-  // Throw an error if the index is greater than the current count
-
-  // Otherwise, return the element at the given index
+  
 }
 
 
 /*****
  * Insert an element to the array at the given index
  *****/
-void arr_insert(Array *arr, char *element, int index) {
-
-  // Throw an error if the index is greater than the current count
-
-  // Resize the array if the number of elements is over capacity
-
-  // Move every element after the insert index to the right one position
-
-  // Copy the element and add it to the array
-
-  // Increment count by 1
+void arr_insert(Array *arr, char *element, int index) {  
 
 }
 
@@ -98,13 +80,6 @@ void arr_insert(Array *arr, char *element, int index) {
  * Append an element to the end of the array
  *****/
 void arr_append(Array *arr, char *element) {
-
-  // Resize the array if the number of elements is over capacity
-  // or throw an error if resize isn't implemented yet.
-
-  // Copy the element and add it to the end of the array
-
-  // Increment count by 1
 
 }
 
@@ -114,15 +89,7 @@ void arr_append(Array *arr, char *element) {
  *
  * Throw an error if the value is not found.
  *****/
-void arr_remove(Array *arr, char *element) {
-
-  // Search for the first occurence of the element and remove it.
-  // Don't forget to free its memory!
-
-  // Shift over every element after the removed element to the left one position
-
-  // Decrement count by 1
-
+void arr_remove(Array *arr, char *element) {  
 }
 
 
