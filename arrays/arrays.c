4a265862da7ca5d1490812b323ffa1533aa825f5
mparedes003 <michelle.g.paredes@gmail.com>
2019-01-14 12:45:14 -0800

    Worked on create_array() and destroy_array() in arrays.c

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -21,11 +21,15 @@ typedef struct Array {
  *****/
 Array *create_array (int capacity) {
   // Allocate memory for the Array struct
-
+    Array * arr = malloc(sizeof(Array));
   // Set initial values for capacity and count
+    arr->capacity = capacity;
+    arr->count = 0;
 
   // Allocate memory for elements
+    arr->elements = malloc(sizeof(char *));
 
+    return arr;
 }
 
 
@@ -35,9 +39,10 @@ Array *create_array (int capacity) {
 void destroy_array(Array *arr) {
 
   // Free all elements
+  free(arr->elements);
 
   // Free array
-
+  free(arr);
 }
 
 /*****
