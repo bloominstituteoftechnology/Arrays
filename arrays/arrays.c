b031f93349bd3324265eace756ef00f0897e100e
Sasha <taylork85@gmail.com>
2019-01-14 16:13:12 -0600

    initial commit

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -21,7 +21,7 @@ typedef struct Array {
  *****/
 Array *create_array (int capacity) {
   // Allocate memory for the Array struct
-
+    
   // Set initial values for capacity and count
 
   // Allocate memory for elements
