81ec8904d4b6c2c2a0315b42ee81c584b7ec593e
jlaluces123 <jlalucescareer123@gmail.com>
2019-01-14 13:26:25 -0800

    Day 1 - Array Read Done

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -79,7 +79,13 @@ void resize_array(Array *arr) {
  * Throw an error if the index is out of range.
  *****/
 char *arr_read(Array *arr, int index) {
-  
+  // Throw an error if the index is greater than the current count
+  if (index < arr->count) {
+    fprintf(stderr, "Index greater than count of array (read)\n");
+    exit(1);
+  }
+  // Otherwise, return the element at the given index
+  return arr->elements[index];  
 }
 
 
