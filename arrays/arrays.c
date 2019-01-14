4e8577e1c2883ca654f9bdfd9895324c7f012d9d
Anthony Greb <agreb255@gmail.com>
2019-01-14 16:45:02 -0500

    create array

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -16,15 +16,18 @@ typedef struct Array {
  *
  ************************************/
 
-/*****
- * Allocate memory for a new array
- *****/
+
+ 
+ 
 Array *create_array (int capacity) {
-  // Allocate memory for the Array struct
+  Array *arr = malloc(sizeof(Array));
 
-  // Set initial values for capacity and count
+  arr->capacity = capacity;
+  arr->count = 0;
 
-  // Allocate memory for elements
+  arr->elements = malloc(capacity + sizeof(char *));
+
+  return arr;
 
 }
 
@@ -34,9 +37,7 @@ Array *create_array (int capacity) {
  *****/
 void destroy_array(Array *arr) {
 
-  // Free all elements
-
-  // Free array
+  
 
 }
 
