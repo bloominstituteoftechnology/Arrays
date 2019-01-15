87690e9e792982ad62cbe5de7bb00c96d87cceb0
verydecent <wnjhwng@gmail.com>
2019-01-15 15:41:26 -0800

    arr append

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -105,11 +105,16 @@ void arr_append(Array *arr, char *element) {
 
   // Resize the array if the number of elements is over capacity
   // or throw an error if resize isn't implemented yet.
-
+  
+  if (arr->capacity <= arr->count) {
+      fprintf(stderr, "IndexError: Index is out of range");
+      return;
+  }
   // Copy the element and add it to the end of the array
+  char *new_element = strdup(element);
 
   // Increment count by 1
-
+  arr->count++;
 }
 
 /*****
