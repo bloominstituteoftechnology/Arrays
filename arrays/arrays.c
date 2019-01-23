6dbb5bef51055688b33e0a5751593035e9e1c8f0
Linda Yang <lindayang@Lindas-iMac.fios-router.home>
2019-01-23 18:00:22 -0500

    Modified arr-insert for loop

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -111,13 +111,13 @@ void arr_insert(Array *arr, char *element, int index) {
   }
 
   // Resize the array if the number of elements is over capacity
-  if (arr->count + 1 > arr->capacity) {
+  if (arr->count  >= arr->capacity) {
     resize_array(arr);
   }
 
   // Move every element after the insert index to the right one position
-  for (int i = index; i < arr->count; i++) {
-    arr->elements[i + 1] = arr->elements[i];
+  for (int i = arr->count; i > index; i--) {
+    arr->elements[i] = arr->elements[i - 1];
   }
 
   // Copy the element and add it to the array
