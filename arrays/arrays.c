1a7967121cf2b7f17bf0ae35a94076e7116b6dc0
cubsfan46350 <cubsfan.46350@msn.com>
2019-01-23 03:46:05 -0600

    initial commit

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -21,7 +21,9 @@ typedef struct Array {
  *****/
 Array *create_array (int capacity) {
   // Allocate memory for the Array struct
-
+  Array *array;
+  int i;
+  array = malloc(capacity*sizeof(Array));
   // Set initial values for capacity and count
 
   // Allocate memory for elements
