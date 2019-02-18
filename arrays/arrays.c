cd35de71f32e4df7641b25f14b0465f5fb41b967
Andrew <ghobrial.andrew@gmail.com>
2019-02-18 17:43:48 -0500

    arr_append

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -112,11 +112,11 @@ void arr_append(Array *arr, char *element) {
 
   // Resize the array if the number of elements is over capacity
   // or throw an error if resize isn't implemented yet.
-
+  char *copy = (char *)element;
   // Copy the element and add it to the end of the array
-
+  array->elements[array->count] = copy;
   // Increment count by 1
-
+  array->count++;
 }
 
 /*****
