7a033ee031cb44f07d9a7490aa707dcf6eae5eaf
Drew <drewsup123c@gmail.com>
2019-02-18 15:39:43 -0700

    MVP : fixed final error

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -125,7 +125,7 @@ void arr_append(Array *arr, char *element) {
       resize_array(arr); //checks if count with another element will go over current capacity, if it is call resize
     }
   // Copy the element and add it to the end of the array
-  char copy = element;
+  char *copy = element;
   arr->elements[arr->count] = copy;
   // Increment count by 1
   arr->count = arr->count + 1;
