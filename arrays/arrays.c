d9ac4bfb67788814ce1edbf9dbad32bc2597738f
Austinlp4 <apendergrast44@gmail.com>
2019-02-18 13:44:32 -0600

    complete create_array function

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -21,11 +21,14 @@ typedef struct Array {
  *****/
 Array *create_array (int capacity) {
   // Allocate memory for the Array struct
+  Array *arr = malloc(sizeof(Array));
 
   // Set initial values for capacity and count
+  arr -> capacity = capacity;
+  arr -> count = 0;
 
   // Allocate memory for elements
-
+  arr -> elements = calloc(capacity, sizeof(char *));
 }
 
 
