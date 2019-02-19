0f29603fa9fd7fe512c7f63e1e2830287675eb38
Hunter <huntersmith_15@hotmail.com>
2019-02-19 11:18:40 -0700

    refactored insert method

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -115,14 +115,14 @@ void arr_insert(Array *arr, char *element, int index)
     exit(1);
   }
   // Resize the array if the number of elements is over capacity
-  if (arr->count + 1 > arr->capacity)
+  if (arr->count >= arr->capacity)
   {
     resize_array(arr);
   }
   // Move every element after the insert index to the right one position
-  for (int i = index; i < arr->count; i++)
+  for (int i = arr->count; i > index; i--)
   {
-    arr->elements[i + 1] = arr->elements[i];
+    arr->elements[i] = arr->elements[i-1];
   }
   // Copy the element and add it to the array
   char *new_element = strdup(element);
