549b284bf1a34cd79d9ecb001757d5eaf7b3e866
Edwin Torres <edwin.torres.jr2@gmail.com>
2019-02-19 17:31:16 -0500

    insert

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -56,16 +56,19 @@ void destroy_array(Array *arr)
  *****/
 void resize_array(Array *arr)
 {
-
   // Create a new element storage with double capacity
-
+  char **new_elements = realloc(arr->elements, arr->capacity * sizeof(char) * 2);
   // Copy elements into the new storage
-
+  for (int i = 0; i < arr->count; i++)
+  {
+    new_elements[i] = arr->elements[i];
+  }
+  // new_elements[arr->count] = '\0';
   // Free the old elements array (but NOT the strings they point to)
-
+  free(arr->elements);
   // Update the elements and capacity to new values
+  arr->elements = new_elements;
 }
-
 /************************************
  *
  *   ARRAY FUNCTIONS
@@ -94,6 +97,36 @@ char *arr_read(Array *arr, int index)
 /*****
  * Insert an element to the array at the given index
  *****/
+void arr_insert(Array *arr, char *element, int index)
+{
+
+  // Throw an error if the index is greater than the current count
+  if (index > arr->count)
+  {
+    return "index out of range";
+  }
+  // Resize the array if the number of elements is over capacity
+  if (arr->count == arr->capacity)
+  {
+    resize_array(arr);
+  }
+  // Move every element after the insert index to the right one position
+  for (int i = arr->count - 1; i >= index; i--)
+  {
+    arr->elements[i + 1] = arr->elements[i];
+  }
+
+  // Copy the element and add it to the array
+  char *new_element = element;
+  arr->elements[index] = new_element;
+
+  // Increment count by 1
+  arr->count++;
+}
+
+/*****
+ * Append an element to the end of the array
+ *****/
 void arr_append(Array *arr, char *element)
 {
 
@@ -113,20 +146,6 @@ void arr_append(Array *arr, char *element)
     // Increment count by 1
     arr->count++;
   }
-}
-
-/*****
- * Append an element to the end of the array
- *****/
-void arr_append(Array *arr, char *element)
-{
-
-  // Resize the array if the number of elements is over capacity
-  // or throw an error if resize isn't implemented yet.
-
-  // Copy the element and add it to the end of the array
-
-  // Increment count by 1
 }
 
 /*****
