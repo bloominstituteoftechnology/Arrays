1cc241dd5db967e5aebb8f122f72e4995a168517
michaelagard <mdagard@gmail.com>
2019-02-25 18:04:32 -0500

    comment out unused functions, change arr_append else if to else

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -56,17 +56,17 @@ void destroy_array(Array *arr)
  * Create a new elements array with double capacity and copy elements
  * from old to new
  *****/
-void resize_array(Array *arr)
-{
+// void resize_array(Array *arr)
+// {
 
-  // Create a new element storage with double capacity
+// Create a new element storage with double capacity
 
-  // Copy elements into the new storage
+// Copy elements into the new storage
 
-  // Free the old elements array (but NOT the strings they point to)
+// Free the old elements array (but NOT the strings they point to)
 
-  // Update the elements and capacity to new values
-}
+// Update the elements and capacity to new values
+// }
 
 /************************************
  *
@@ -96,20 +96,20 @@ char *arr_read(Array *arr, int index)
 
 /*****
  * Insert an element to the array at the given index
- *****/
-void arr_insert(Array *arr, char *element, int index)
-{
+*****/
+// void arr_insert(Array *arr, char *element, int index)
+// {
 
-  // Throw an error if the index is greater than the current count
+// Throw an error if the index is greater than the current count
 
-  // Resize the array if the number of elements is over capacity
+// Resize the array if the number of elements is over capacity
 
-  // Move every element after the insert index to the right one position
+// Move every element after the insert index to the right one position
 
-  // Copy the element and add it to the array
+// Copy the element and add it to the array
 
-  // Increment count by 1
-}
+// Increment count by 1
+// }
 
 /*****
  * Append an element to the end of the array
@@ -124,7 +124,7 @@ void arr_append(Array *arr, char *element)
   }
 
   // or throw an error if resize isn't implemented yet.
-  else if (resize_array)
+  else
   {
     printf("resize_array() not yet implemented");
   }
@@ -141,16 +141,16 @@ void arr_append(Array *arr, char *element)
  *
  * Throw an error if the value is not found.
  *****/
-void arr_remove(Array *arr, char *element)
-{
+// void arr_remove(Array *arr, char *element)
+// {
 
-  // Search for the first occurence of the element and remove it.
-  // Don't forget to free its memory!
+// Search for the first occurence of the element and remove it.
+// Don't forget to free its memory!
 
-  // Shift over every element after the removed element to the left one position
+// Shift over every element after the removed element to the left one position
 
-  // Decrement count by 1
-}
+// Decrement count by 1
+// }
 
 /*****
  * Utility function to print an array.
@@ -178,7 +178,7 @@ int main(void)
   // arr_insert(arr, "STRING1", 0);
   arr_append(arr, "STRING4");
   // arr_insert(arr, "STRING2", 0);
-  // arr_insert(arr, "STRING3", 1);
+  // arr_insert(arr, "STRING3/", 1);
   arr_print(arr);
   // arr_remove(arr, "STRING3");
   arr_print(arr);
