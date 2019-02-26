2586db9f9da10d5741e4672c970f3245e12b45ec
Barbara Hunter <commandandcode@gmail.com>
2019-02-26 17:50:27 -0500

    done

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -89,43 +89,72 @@ void resize_array(Array *arr) {
  *
  * Throw an error if the index is out of range.
  *****/
-char *arr_read(Array *arr, int index) {
+char *arr_read(Array *arr, int index)
+{
+  if (arr->count > index)
+  {
+    printf("Error! that index doesn't exist.");
+  }
+  else
+  {
+    return arr->elements[index];
+  }
 
   // Throw an error if the index is greater than the current count
 
   // Otherwise, return the element at the given index
 }
 
-
 /*****
  * Insert an element to the array at the given index
  *****/
-void arr_insert(Array *arr, char *element, int index) {
+void arr_insert(Array *arr, char *element, int index)
+{
 
   // Throw an error if the index is greater than the current count
+  if (index > arr->count)
+  {
+    fprintf(stderr, "Index is beyond array bounds.");
+    exit(1);
+  }
 
   // Resize the array if the number of elements is over capacity
+  if (arr->count + 1 > arr->capacity)
+  {
+    resize_array(arr);
+  }
 
   // Move every element after the insert index to the right one position
+  for (int i = index; i < arr->count; i++)
+  {
+    arr->elements[i + 1] = arr->elements[i];
+  }
 
   // Copy the element and add it to the array
+  arr->elements[index] = element;
 
   // Increment count by 1
-
+  arr->count++;
 }
 
 /*****
  * Append an element to the end of the array
  *****/
-void arr_append(Array *arr, char *element) {
+void arr_append(Array *arr, char *element)
+{
 
   // Resize the array if the number of elements is over capacity
   // or throw an error if resize isn't implemented yet.
+  if (arr->count + 1 > arr->capacity)
+  {
+    resize_array(arr);
+  }
 
   // Copy the element and add it to the end of the array
+  arr->elements[arr->count] = element;
 
   // Increment count by 1
-
+  arr->count++;
 }
 
 /*****
@@ -134,32 +163,54 @@ void arr_append(Array *arr, char *element) {
  *
  * Throw an error if the value is not found.
  *****/
-void arr_remove(Array *arr, char *element) {
+void arr_remove(Array *arr, char *element)
+{
+  int index = 0;
 
   // Search for the first occurence of the element and remove it.
-  // Don't forget to free its memory!
+  for (int i = 0; i < arr->count; i++)
+  {
+    if (arr->elements[i] == element)
+    {
+      index = i;
 
-  // Shift over every element after the removed element to the left one position
+      // Don't forget to free its memory!
+      arr->elements[i] = NULL;
+      free(arr->elements[i]);
 
-  // Decrement count by 1
+      // Shift over every element after the removed element to the left one position
+      for (int i = index; i < arr->count; i++)
+      {
+        arr->elements[i] = arr->elements[i + 1];
+      }
 
+      // Decrement count by 1
+      arr->count--;
+    }
+    else if (i == arr->count)
+    {
+      fprintf(stderr, "Element not found.");
+      exit(1);
+    }
+  }
 }
-
 
 /*****
  * Utility function to print an array.
  *****/
-void arr_print(Array *arr) {
+void arr_print(Array *arr)
+{
   printf("[");
-  for (int i = 0 ; i < arr->count ; i++) {
+  for (int i = 0; i < arr->count; i++)
+  {
     printf("%s", arr->elements[i]);
-    if (i != arr->count - 1) {
+    if (i != arr->count - 1)
+    {
       printf(",");
     }
   }
   printf("]\n");
 }
-
 
 #ifndef TESTING
 int main(void)
